26a593d13b68140fd3ff68ccfe2f03fbaac6787b
xf00889 <hutchiejn@gmail.com>
2025-02-14 03:18:48 +0800

    Add explicit INSTALLED_APPS configuration

--- valentines_project/settings_prod.py
+++ valentines_project/settings_prod.py
@@ -5,6 +5,22 @@ DEBUG = True  # Temporarily set to True to see errors
 # Updated with your PythonAnywhere username
 ALLOWED_HOSTS = ['hutchie.pythonanywhere.com']
 
+# Explicitly include installed apps
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'valentines_app',
+    'crispy_forms',
+    'crispy_bootstrap5',
+]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 # Database settings
 DATABASES = {
     'default': {
